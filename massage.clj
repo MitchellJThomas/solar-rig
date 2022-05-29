c24e914c346d304b72779b49bce86ad93bf4ad7b
Mitch Thomas <mthomas@tripwire.com>
2022-05-29 11:53:06 -0700

    Use Influx Point

--- massage.clj
+++ massage.clj
@@ -5,8 +5,10 @@
      [java-time :as t]
      )
     (:import
+     (com.influxdb.client.write Point)
      (com.influxdb.client.domain WritePrecision)
-     (com.influxdb.client InfluxDBClientFactory))
+     (com.influxdb.client InfluxDBClientFactory)
+     )
     )
 
 (defn concat-files
@@ -49,19 +51,25 @@
    )
   )
 
-
 (defn influx-field [k v]
   (str k "=" v))
 
 (defn influx-tag [k v]
-  (str "," k "=" v))
+  (str k "=\"" v "\""))
 
 (defn to-ts [date time]
-  ;; Expects time in the format 2020/02/01,00:05:44  
+  ;; Expects time in the format 2020/02/01 and 00:05:44  
+  (-> (t/local-date-time "yyyy/MM/dd HH:mm:ss" (str date " " time))
+      (t/zoned-date-time (t/zone-id "America/Los_Angeles"))
+      (t/offset-date-time)      
+      (t/to-millis-from-epoch)))
+
+(defn to-instant [date time]
+  ;; Expects time in the format 2020/02/01 and 00:05:44  
   (-> (t/local-date-time "yyyy/MM/dd HH:mm:ss" (str date " " time))
       (t/zoned-date-time (t/zone-id "America/Los_Angeles"))
       (t/offset-date-time)
-      (t/to-millis-from-epoch)))
+      (t/instant)))
 
 (defn floaty [str-float]
   (try
@@ -71,32 +79,49 @@
         (Float/parseFloat (string/replace str-float #"<" ""))
         (catch Exception e str-float)))))
 
-(defn influx-fields [field-key-map]
+(defn influx-fields-str [field-key-map]
   (apply str (interpose "," (for [[k v] field-key-map :when (float? v)] (influx-field k v))))
   )
+
+(defn influx-point-fields [point field-key-map]
+  (reduce #(.addField %1 (first %2) (second %2)) point (seq field-key-map)))
 
 (defn influxify
   "Take a sequence of lines in csv format. The first line is the column
   headers and the remainder is the associated data for each
-  column. Convert lines to the Influx Line Protocol so that it can be
+  column. Convert lines to the Influx Point so that it can be
   imported into an InfluxDB instance"
   [measurement-name csv-line-seq]
   (let [column-names (map format-column-name (string/split (first csv-line-seq) #","))
         rows (map #(string/split % #",") (drop 1 csv-line-seq))
         cells (for [row rows]
                 (into {} (map #(vector %1 (floaty %2)) column-names row)))
-        with-ts (map #(assoc % "timestamp" (to-ts (% "date")  (% "time"))) cells)
-        wo-date-time (map #(dissoc % "date" "time") with-ts)
+        with-instant (map #(assoc % "timestamp" (to-instant (% "date")  (% "time"))) cells)
+        wo-date-time (map #(dissoc % "date" "time") with-instant)
         ]
-    (map #(str measurement-name
-               (influx-tag "location" "garage") " "
-               (influx-fields %) " "
-               (% "timestamp")) wo-date-time) 
+    
+    (map #(->
+           (new Point measurement-name)
+           (.addTag "location" "garage")
+           (.addTag "battery" "Relion RB100")
+           (.addTag "load1" "garage door")
+           (.addTag "load2" "PurpleAir sensor")
+           (influx-point-fields (dissoc % "timestamp"))
+           (.time (% "timestamp") WritePrecision/MS))
+         wo-date-time)
+    ;; (map #(str measurement-name ","
+    ;;            (influx-tag "location" "garage") ","
+    ;;            (influx-tag "battery" "Relion RB100") ","
+    ;;            (influx-tag "load1" "garage door") ","
+    ;;            (influx-tag "load2" "purple air sensor") " "
+    ;;            (influx-fields %) " "
+    ;;            (% "timestamp")) wo-date-time) 
     )
   )
   
 
 (comment
+  
   (str "," "foo" "=" "bar", " ", "x")
   ;; user:solarrig
   ;; password:the usual
@@ -104,24 +129,36 @@
         url "http://localhost:8086"
         org "solar-rigs"
         bucket "samlex-evo-2012"
-        precision WritePrecision/NS]
+        precision WritePrecision/MS]
     (with-open [ic (InfluxDBClientFactory/create url (char-array solarrig-token))]
       (let [wtr (.getWriteApiBlocking ic)
             append-line (fn [line] (.writeRecord wtr bucket org precision line))]
         (append-line (str "invertor-sample"
-                          (influx-tag "tagKey" "tagValue")
-                          (influx-field "genStatus" (rand 100))
-                          (to-ts "2000/02/02,10:47:02"))))
+                          (influx-tag "tagKey" "tagValue") " "
+                          (influx-field "genStatus" (rand 100)) " "
+                          (to-ts "2022/05/01" "10:47:02"))))))
+    
+  (let [solarrig-token "q919gL53Udy569RVtLY8K2E6efrBjGuxa17RV-ylRI40puYJLFw_EkkrPyP_TuNkof6vwmAIWpSzeOfcZDz3-Q=="
+        url "http://localhost:8086"
+        org "solar-rigs"
+        bucket "samlex-evo-2012"]
+    (with-open [ic (InfluxDBClientFactory/create url (char-array solarrig-token))
+                rdr (io/reader "/Users/mthomas/Dev/solar-rig-data/test.csv")]
+      (let [wtr (.getWriteApiBlocking ic)
+            write-point (fn [point]
+                          (println "Writing point" (.toLineProtocol point))
+                          (.writePoint wtr bucket org point)
+                          )]
+        (doseq [point (influxify "a-inverter-sample" (line-seq rdr))]
+          (write-point point))
+        )
       )
     )
-  
-  ;; Date,Time,Gen status,Gen freq,Gen volt,Grid status,Grid freq,Grid volt,Input current,Input VA,Input watt,Output freq,Output volt,Output current,Output VA,Output watt,Battery volt,Battery current,External current,Battery temperature(C),Transformer temperature(C),Bus bar temperature(C),Heat sink temperature(C),Fan speed,Mode,Error code,Charge stage,Event,
-  ;; 2000/02/01,00:05:44,49725,000.00,000.43,49727,000.00,000.24,<00.10,<0012,<0012,000.00,000.27,<00.10,<0012,<0012,13.382,0000.0,0000.0,0025.0,0007.9,0010.9,0010.6,0,0,00000,0,
 
   (def dir "/Users/mthomas/Dev/solar-rig-data/data")
-  (concat-files dir "03" "mar-2020.csv")
-  (concat-files dir "02" "feb-2020.csv")
-  (concat-files dir "01" "jan-2020.csv")
+  (concat-files dir "03" "mar-2022.csv")
+  (concat-files dir "02" "feb-2022.csv")
+  (concat-files dir "01" "jan-2022.csv")
 
   (concat-files dir "1027040" "test.csv")
 
